c0142a02a58504108b26e08c4a2db3dd9b63cd8f
MakaveliMartinez <48743766+MakaveliMartinez@users.noreply.github.com>
2024-12-08 14:28:42 -0800

    Update Student.py
    Basic student info with ID etc created a list of field of courses so that students can have a list of courses they're taking, Included in the clean is a way to check if they exceed the maximum number of units they can take at one time which is 18

--- InSQL/Student.py
+++ InSQL/Student.py
@@ -1,8 +1,34 @@
 from mongoengine import *
 
+from Course import Course
+
+
 class Student(Document):
     lastName = StringField(db_field='last_name', min_length=4, max_length=90, required=True)
     firstName = StringField(db_field='first_name', min_length=4, max_length=90, required=True)
     studentID = IntField(min_value=0, max_value=10, required=True, unique=True)
 
+    courses = ListField(ReferenceField('Course'), db_field='courses_taken')
 
+
+    def clean(self):
+        """
+        Making sure students don't exceed the 18 unit Max, probably need to keep track of prior takin courses
+        """
+
+        total_units = sum(course.unit for course in self.courses)
+        if total_units > 18:
+            raise ValidationError(f"Total units {total_units} exceeds the max amount of 18 units")
+
+    def __init__ (self, lastName, firstName,studentID,courses= None,**kwargs):
+        super().__init__(**kwargs)
+        self.firstName = firstName
+        self.lastName = lastName
+        self.studentID = studentID
+        self.courses = courses or []
+
+
+
+    def __str__(self):
+        course_names = [course.courseName for course in self.courses] if self.courses else "None"
+        return f"Student - name: {self.firstName}  {self.lastName}, ID#: {self.studentID}, Courses: {course_names}"
