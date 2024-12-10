e7a3066dd5f9394ab36f3ddfef94da7a8e8c4b04
MakaveliMartinez <48743766+MakaveliMartinez@users.noreply.github.com>
2024-12-10 00:06:52 -0800

    Update Requirements.py
    Just an idea for what it should be will work on clarifying it more tomorrow,.

--- InMongo/Requirements.py
+++ InMongo/Requirements.py
@@ -3,6 +3,23 @@ from mongoengine import *
 
 class Requirements(Document):
     """What is Required to take a course"""
-    units = IntField(required=True)
+    degree_type = ReferenceField('DegreeCatalog', required=True)
+    name = StringField(min_length=10, max_length=30, required=True) # Truthfully I dot know what name is supposed to be just saw it in the vertabelo and added it
+    total_point = IntField(required=True)
+
+    meta = {
+        'collection': 'requirements',
+        'indexes': [
+            {'fields': ['degree_type', 'name'], 'unique': True}
+        ]
+    }#This pretty much creates a unique composite with the degree type FK and the name as a Pk think composite key
+
+    def __init__ (self, degree_type, name,total_point, **kwargs):
+        super().__init__(**kwargs)
+        self.degree_type= degree_type
+        self.name = name
+        self.total_point = total_point
 
 
+    def __str__(self):
+        return f'Degree Type {self.degree_type}, Name  {self.name}, Total Points {self.total_point}'
